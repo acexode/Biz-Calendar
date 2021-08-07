824f4553fbb9967ac8182bcdef4523bec276d28d
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-08-07 15:12:41 +0100

    feat: completed input

--- src/app/style-guide/input-config-data/input-config-data.ts
+++ src/app/style-guide/input-config-data/input-config-data.ts
@@ -7,6 +7,7 @@ interface InputStyleGuideCong {
   focusedConfig?: IonInputConfig;
   errorConfig?: IonInputConfig;
   defaultWithIcon?: IonInputConfig;
+  filledWithIcon?: IonInputConfig;
   disabledConfigWithIcon?: IonInputConfig;
   focusedConfigWithIcon?: IonInputConfig;
   errorConfigWithIcon?: IonInputConfig;
@@ -47,7 +48,7 @@ export const inputStyleGuideConfigurations: InputStyleGuideCong = {
     disabled: true,
   },
   filledConfig: {
-    placeholder: 'Cannot edit',
+    placeholder: 'Type Here',
     type: 'text',
     inputMode: 'text',
     size: 100,
@@ -100,7 +101,7 @@ export const inputStyleGuideConfigurations: InputStyleGuideCong = {
     inputHasError: true,
   },
   defaultWithIcon: {
-    placeholder: 'Type here',
+    placeholder: 'PlaceHolder',
     type: 'text',
     inputMode: 'text',
     size: 100,
@@ -117,6 +118,88 @@ export const inputStyleGuideConfigurations: InputStyleGuideCong = {
     disabled: false,
     useIcon: {
       name: 'chevron-down'
+    }
+  },
+  filledWithIcon: {
+    placeholder: 'Type Here',
+    type: 'text',
+    inputMode: 'text',
+    size: 100,
+    inputLabel: {
+      text: 'Filled',
+      classes: '',
+      slot: '',
+    },
+    clearable: false,
+    inputClasses: '',
+    minLength: 10,
+    maxLength: 10,
+    bgwhite: true,
+    disabled: false,
+    useIcon: {
+      name: 'chevron-down'
+    }
+  },
+  disabledConfigWithIcon: {
+    placeholder: 'Cannot edit',
+    type: 'email',
+    inputMode: 'email',
+    size: 100,
+    inputLabel: {
+      text: 'Disabled',
+      classes: '',
+      slot: '',
+    },
+    clearable: false,
+    inputClasses: '',
+    minLength: 10,
+    maxLength: 10,
+    bgwhite: true,
+    disabled: true,
+    useIcon: {
+      name: 'chevron-down'
+    }
+  },
+  focusedConfigWithIcon: {
+    placeholder: 'Type here',
+    type: 'email',
+    inputMode: 'email',
+    size: 100,
+    inputLabel: {
+      text: 'Focused',
+      classes: '',
+      slot: '',
+    },
+    clearable: false,
+    inputClasses: '',
+    minLength: 10,
+    maxLength: 10,
+    bgwhite: true,
+    disabled: false,
+    isInputFocused: true,
+    useIcon: {
+      name: 'chevron-down'
+    }
+  },
+  errorConfigWithIcon: {
+    placeholder: 'Type here',
+    type: 'email',
+    inputMode: 'email',
+    size: 100,
+    inputLabel: {
+      text: 'Error',
+      classes: '',
+      slot: '',
+    },
+    clearable: false,
+    inputClasses: '',
+    minLength: 10,
+    maxLength: 10,
+    bgwhite: true,
+    disabled: false,
+    inputHasError: true,
+    useIcon: {
+      name: 'warning-custom'
     }
   },
 };
