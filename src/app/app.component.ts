20081913e79a5587c45690781c968dc53f220974
Sir Abubakar <abubakar.abdulwahab@outsourceglobal.com>
2021-08-09 08:03:59 +0100

    tablet view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-
-  constructor() {}
+  isTablet = false;
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      console.log('Width: ' + platform.width());
+      if(platform.width() >= 768){
+        this.isTablet = true;
+      }else{
+        this.isTablet = false;
+      }
+      console.log('Height: ' + platform.height());
+    });
+  }
 }
