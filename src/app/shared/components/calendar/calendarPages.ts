b1b28209adfa81d1715865da52e0f1ed31154ae3
Sir Abubakar <abubakar.abdulwahab@outsourceglobal.com>
2021-08-11 07:30:04 +0100

    list view, day view

---
+++ src/app/shared/components/calendar/calendarPages.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const CalendarPages = [
+    { title: 'Listă', url: '/list', icon: '1-day' },
+    { title: 'Zi', url: '/Day', icon: 'schedule' },
+    { title: 'Săptămână', url: '/Week', icon: '5-days' },
+    { title: 'Lună', url: '/Month', icon: 'Month' },
+    { title: 'Comparativ', url: '/comparative', icon: 'coparativ' },
+];
