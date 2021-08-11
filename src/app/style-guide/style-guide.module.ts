7e09e31d147ccc2adb25d0b08b7e37b5d782cbbb
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-08-11 21:46:18 +0100

    feat: tooltips

--- src/app/style-guide/style-guide.module.ts
+++ src/app/style-guide/style-guide.module.ts
@@ -9,6 +9,7 @@ import { StyleGuidePageRoutingModule } from './style-guide-routing.module';
 import { StyleGuidePage } from './style-guide.page';
 import { BizInputsModule } from '../shared/modules/biz-inputs/biz-inputs.module';
 import { SelectionComponent } from './components/selection/selection.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
@@ -17,8 +18,8 @@ import { SelectionComponent } from './components/selection/selection.component';
     ReactiveFormsModule,
     IonicModule,
     StyleGuidePageRoutingModule,
-    BizInputsModule
+    SharedModule
   ],
   declarations: [StyleGuidePage, SelectionComponent]
 })
-export class StyleGuidePageModule {}
+export class StyleGuidePageModule { }
