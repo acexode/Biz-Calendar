ce000ed7f8872acb4269c9241d4f2dc09775f496
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-08-17 16:20:59 +0100

    feat: patient form

--- src/app/home/adauga-programare/adauga-programare.component.ts
+++ src/app/home/adauga-programare/adauga-programare.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { inputConfigHelper } from 'src/app/shared/data/input-config-helper';
+import { inputStyleGuideConfigurations } from 'src/app/style-guide/input-config-data/input-config-data';
 
 @Component({
   selector: 'app-adauga-programare',
@@ -6,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adauga-programare.component.scss'],
 })
 export class AdaugaProgramareComponent implements OnInit {
-
-  constructor() { }
+  inputStyleGuide = inputStyleGuideConfigurations;
+  pat = inputConfigHelper({
+    label: 'Pacient',
+    type: 'text',
+    placeholder: 'Value'
+  });
+  adaugaProgramareFormGroup: FormGroup = this.fb.group({
+    pacient: ['', [Validators.required]],
+  });
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {}
 
---
+++ src/app/shared/data/input-config-helper.ts
@@ -0,0 +1,28 @@
+
+import { get, has, assignIn } from 'lodash';
+import { IonInputConfig } from '../models/components/ion-input-config';
+
+export const inputConfigHelper = (conf: {
+  label: string;
+  type: string;
+  placeholder: string;
+  disabled?: boolean;
+  custom?: IonInputConfig;
+  inputMode?: string;
+}): IonInputConfig => {
+  const config: IonInputConfig = {
+    type: get(conf, 'type', 'text'),
+    placeholder: get(conf, 'placeholder', ''),
+    inputLabel: {
+      text: get(conf, 'label', ''),
+      classes: 'mb-8 input-label',
+    },
+    disabled: conf.disabled || false,
+    inputMode: get(conf, 'inputMode', 'none'),
+  };
+  if (has(conf, 'custom')) {
+    return assignIn(config, get(conf, 'custom', {}));
+  } else {
+    return config;
+  }
+};
