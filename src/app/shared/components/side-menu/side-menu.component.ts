ee52ec7829e4efe0e647e82ac1be2c9d54763ffb
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-08-23 02:05:38 +0100

    feat: added link to sidebar

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -29,7 +29,9 @@ export class SideMenuComponent implements OnInit {
     { title: 'Date statistice', url: '/calendar/pacienti', icon: 'bar-chart', chevron: true, notify:false },
     { title: 'Setări', url: '/calendar/setari', icon: 'settings-custom', chevron: false, notify:false },
     { title: 'Notificări', url: '/calendar/notificari', icon: 'notificare', chevron: false, notify:true  },
-    { title: 'Ieșire', url: '/calendar/lesire', icon: 'deconectare', chevron: false, notify:false },
+    { title: 'Ieșire', url: '/calendar/lesire', icon: 'deconectare', chevron: false, notify: false },
+    { title: 'style Guide', url: '/style', icon: 'edit', chevron: false, notify: false },
+    { title: 'calendar adauga-programare', url: '/calendar/adauga-programare', icon: 'edit', chevron: false, notify: false },
     // { title: 'Comparativ', url: '/calendar/comparativ', icon: 'coparativ' },
   ];
   constructor(platform: Platform, private menu: MenuController) {
