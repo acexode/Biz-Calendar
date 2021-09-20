9f2749cc93f59c2dd8cb1eb0379b2b7027483303
Ereyomi Oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2021-09-20 13:18:11 +0100

    feat:  get cities and countries

--- src/app/shared/components/modal/new-pacient-modal/new-pacient-modal.component.ts
+++ src/app/shared/components/modal/new-pacient-modal/new-pacient-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { RequestService } from 'src/app/core/services/request/request.service';
@@ -7,13 +7,16 @@ import { IonRadioInputOption } from 'src/app/shared/models/components/ion-radio-
 import { IonRadiosConfig } from 'src/app/shared/models/components/ion-radios-config';
 import { IonSelectConfig } from 'src/app/shared/models/components/ion-select-config';
 import { get } from 'lodash';
-import { persons } from 'src/app/core/configs/endpoints';
+import { persons, location } from 'src/app/core/configs/endpoints';
+import { Subscription } from 'rxjs';
+import { unsubscriberHelper } from 'src/app/core/helpers/unsubscriber.helper';
+import { distinctUntilChanged } from 'rxjs/operators';
 @Component({
   selector: 'app-new-pacient-modal',
   templateUrl: './new-pacient-modal.component.html',
   styleUrls: ['./new-pacient-modal.component.scss'],
 })
-export class NewPacientModalComponent implements OnInit {
+export class NewPacientModalComponent implements OnInit, OnDestroy {
   @Input() data!: any;
   numeConfig = inputConfigHelper({
     label: 'Nume',
@@ -149,6 +152,7 @@ export class NewPacientModalComponent implements OnInit {
         classes: 'neutral-grey-medium-color'
       }
   };
+  orasOptions: any;
   addMoreField = false;
   componentFormGroup: FormGroup = this.fb.group({
     nume: ['', [Validators.required]],
@@ -158,11 +162,14 @@ export class NewPacientModalComponent implements OnInit {
     telephone: '',
     email: '',
     cnp:'',
-    judet: '',
+    judet: [{value: '', disabled: true}, Validators.required],
     canalDePromovare: '',
-    oras: ''
+    oras: [{value: '', disabled: true}, Validators.required],
   });
   loading = false;
+  addUser$: Subscription;
+  getCountries$: Subscription;
+  getCities$: Subscription;
   constructor(
     private fb: FormBuilder,
     private modalController: ModalController,
@@ -173,8 +180,57 @@ export class NewPacientModalComponent implements OnInit {
     if (this.data) {
       this.componentFormGroup.patchValue(this.data);
     }
+    this.componentFormGroup.get('judet').valueChanges
+      .pipe(
+        distinctUntilChanged()
+      )
+      .subscribe(
+        d => {
+          if (d) {
+            this.getCities(d);
+          }
+        }
+      );
+  }
+  getCountries() {
+    this.getCountries$ = this.reqS.post(location.getCountries, {
+      searchString: '',
+    })
+      .subscribe(
+        (d: any) => {
+          this.judetOption = d.counties.map((res: any) =>
+            ({
+                ...res,
+                label: res.name,
+                id: res.id,
+              }));
+          console.log(this.judetOption);
+          this.componentFormGroup.controls?.judet?.enable();
+        },
+        err => console.log(err)
+      );
+  }
+  getCities(
+    id: number = this.componentFormGroup.value.judet,
+    searchString: string = '') {
+    this.getCities$ = this.reqS.post(location.getCities, {
+      id,
+      searchString,
+    })
+    .subscribe(
+      (d: any) => {
+        this.orasOptions = d.cities.map((res: any) =>
+          ({
+              ...res,
+              label: res.name,
+        }));
+        this.componentFormGroup.controls?.oras?.enable();
+      },
+      err => console.log(err)
+    );
   }
   toggleMoreField() {
+    this.getCountries();
     this.addMoreField = !this.addMoreField;
   }
   toggleLoadingState() {
@@ -186,13 +242,13 @@ export class NewPacientModalComponent implements OnInit {
       const d = {
         firstName: get(this.componentFormGroup.value, 'nume', ''),
         lastName: get(this.componentFormGroup.value, 'preNume', ''),
-        pid: get(this.componentFormGroup.value, 'dateNasterii', ''),
+        pid: get(this.componentFormGroup.value, 'cnp', ''),
         phone: get(this.componentFormGroup.value, 'telephone', ''),
         cityID: Number(get(this.componentFormGroup.value, 'cityId', 0)),
         genderID: Number(get(this.componentFormGroup.value, 'sex', 0)),
         passWarnnings: true
       };
-      this.reqS.post(persons.addPerson, d).subscribe(
+      this.addUser$ = this.reqS.post(persons.addPerson, d).subscribe(
         _rep => {
           this.toggleLoadingState();
           this.closeModal();
@@ -212,6 +268,11 @@ export class NewPacientModalComponent implements OnInit {
   }
   get formGroupValidity() {
     return this.componentFormGroup.valid;
+  }
+
+  ngOnDestroy() {
+    unsubscriberHelper(this.getCountries$);
+    unsubscriberHelper(this.addUser$);
   }
 
 }
