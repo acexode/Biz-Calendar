06ef4832bdc7cc91878f7ac41a5d58933fb5eb52
Ereyomi Oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2021-10-05 11:07:13 +0100

    refactor

--- src/app/home/recurenta/recurenta.component.ts
+++ src/app/home/recurenta/recurenta.component.ts
@@ -200,6 +200,7 @@ export class RecurentaComponent implements OnInit, OnDestroy {
     return this.timeChooseFormControl.value;
   }
   setZileValue(data: string) {
+    console.log(data, this.timeChooseFormControl.value === this.seRepetaLaFiecareOption[0].label);
     this.timeChooseFormControl.setValue(data);
   }
   setZileValueAndToggle(data: string) {
